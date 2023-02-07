0bec81019f4412d770f3a0b1010dfd776ce56dd8
rahul0tripathi <rahultripathidev@gmail.com>
2023-02-07 19:38:04 +0530

    ft: make error check more generic

--- ethereumetl/jobs/export_token_transfers_job.py
+++ ethereumetl/jobs/export_token_transfers_job.py
@@ -79,16 +79,18 @@ class ExportTokenTransfersJob(BaseJob):
             event_filter = self.web3.eth.filter(filter_params)
             events = event_filter.get_all_entries()
         except ValueError as e:
-            if str(e) == "{'code': -32000, 'message': 'the method is currently not implemented: eth_newFilter'}":
+            if str(e).find('implemented: eth_newFilter'):
                 self._supports_eth_newFilter = False
                 events = self.web3.eth.getLogs(filter_params)
             else:
-                raise(e)
+                raise (e)
         for event in events:
             log = self.receipt_log_mapper.web3_dict_to_receipt_log(event)
-            token_transfer = self.token_transfer_extractor.extract_transfer_from_log(log)
+            token_transfer = self.token_transfer_extractor.extract_transfer_from_log(
+                log)
             if token_transfer is not None:
-                self.item_exporter.export_item(self.token_transfer_mapper.token_transfer_to_dict(token_transfer))
+                self.item_exporter.export_item(
+                    self.token_transfer_mapper.token_transfer_to_dict(token_transfer))
 
         if self._supports_eth_newFilter:
             self.web3.eth.uninstallFilter(event_filter.filter_id)
